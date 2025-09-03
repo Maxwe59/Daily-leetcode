b753047018633a191594cefe41b5b763f1cf82ac
Max Wong <110420743+Maxwe59@users.noreply.github.com>
2025-09-02 18:46:13 -0700

    easy.

--- NewSolutions.py
+++ NewSolutions.py
@@ -311,6 +311,16 @@ class Solutions:
             res_arr.append(list(item))
         res_arr.append([])
         return res_arr
-
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        res = 0
+        for i in range(1, len(prices)):
+            if prices[i] < prices[left]:
+                left = i
+            
+            else:
+                res = max(res, (prices[i] - prices[left]))
+        
+        return res
 
 test = Solutions()
